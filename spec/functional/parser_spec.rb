b02a9d84efcbf8f80673e28721188f096347f81b
Sathish <reachtosathish.b@gmail.com>
2022-07-25 01:52:55 +0800

    test expected most views and unique views

--- spec/functional/parser_spec.rb
+++ spec/functional/parser_spec.rb
@@ -2,30 +2,46 @@ require_relative '../spec_helper'
 
 RSpec.describe 'Functional Test' do
   let!(:file) { file_fixture('webserver.log') }
-  let(:expected_op) do
+  let(:expected_most_views) do
     "-------------------------\n"\
-    "/home 3 visits\n"\
-    "/help_page/1 3 visits\n"\
+    "/home 90 visits\n"\
+    "/help_page/1 30 visits\n"\
     "-------------------------\n"\
-    "\n"\
+  end
+
+  let(:expected_unique_views) do
     "-------------------------\n"\
-    "/home 2 unique views\n"\
-    "/help_page/1 2 unique views\n"\
+    "/home 20 unique views\n"\
+    "/help_page/1 20 unique views\n"\
     "-------------------------\n"
   end
+
   let(:parser) { Bundler.root.join('bin', 'parser.rb') }
 
+  subject { `#{parser} webserver.log` }
   describe 'Receives a log as argument' do
     context 'when log file exists' do
       it 'should accept the log file' do
-        expect(`#{parser} webserver.log`).to start_with('File Accepted!')
+        expect(subject).to start_with('File Accepted!')
       end
     end
 
     context 'when log file does not exist' do
       it 'should accept the log file' do
-        expect(`#{parser} webserver.log`).to start_with('File does not exist!')
+        expect(subject).to start_with('File does not exist!')
       end
+    end
+  end
+
+  describe 'List Webpages sorted by most views' do
+    it 'should list webpages by most views' do
+      expect(subject).to include(expected_most_views)
+    end
+  end
+
+  describe 'List Webpages sorted by most unique views' do
+    it 'should list webpages by most unique views' do
+      expect(subject).to include(expected_unique_views)
     end
   end
 end
