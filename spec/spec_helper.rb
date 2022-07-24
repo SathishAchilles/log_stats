a455493243a35b046aa3f089ab54bf1bf0a94884
Sathish <reachtosathish.b@gmail.com>
2022-07-25 01:44:41 +0800

    add file fixture path helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,10 @@
 # it.
 #
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'bundler'
+
+FIXTURE_PATH = Bundler.root.join('spec', 'fixtures').freeze
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
@@ -74,4 +78,8 @@ RSpec.configure do |config|
   # test failures related to randomization by passing the same `--seed` value
   # as the one that triggered the failure.
   Kernel.srand config.seed
+end
+
+def file_fixture(file)
+  FIXTURE_PATH.join(file)
 end
