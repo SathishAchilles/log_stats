28c6b29aa392dde2f752d316c71c79706a469831
Sathish <reachtosathish.b@gmail.com>
2022-08-01 05:38:29 +0800

    allow to skip invalid entries

--- app/services/file_processor.rb
+++ app/services/file_processor.rb
@@ -9,7 +9,9 @@ class FileProcessor < ApplicationService
   def execute
     FileValidator.validate!(file_path)
     File.open(file_path, 'r').each_line do |line|
-      LogEntryProcessor.execute(line)
+      LogEntryProcessor.execute(line.strip)
+    rescue InvalidLogEntryError => e
+      $stderr.write "#{e.message}\n"
     end
   end
 
